78bedf773056c06d9a2cf8829fc877bd3fb476d4
luchao <317494004@qq.com>
2017-04-08 17:23:36 +0800

    modify collect.js file

--- collect.js
+++ collect.js
@@ -1,75 +1,28 @@
-/* JEND.cookie */
-(function($) {
-    if (!$ || !window.ln) return;
-    // ----------------------------
-    ln.namespace('ln.cookie');
-    $.extend(ln.cookie, {
-        getRootDomain: function() {
-            var d = document.domain;
-            if (d.indexOf('.') > 0 && !d.isIP()) {
-                var arr = d.split('.'),
-                    len = arr.length,
-                    d1 = arr[len - 1],
-                    d2 = arr[len - 2],
-                    d3 = arr[len - 3];
-                d = (d2 == 'com' || d2 == 'net') ? (d3 + '.' + d2 + '.' + d1) : (d2 + '.' + d1);
-            }
-            return d;
-        },
-        load: function() {
-            var tC = document.cookie.split('; ');
-            var tO = {};
-            var a = null;
-            for (var i = 0; i < tC.length; i++) {
-                a = tC[i].split('=');
-                tO[a[0]] = a[1];
-            }
-            return tO;
-        },
-        get: function(name) {
-            var value = this.load()[name];
-            if (value) {
-                try {
-                    return decodeURI(value);
-                } catch(e) {
-                    return unescape(value);
-                }
-            } else {
-                return false;
-            }
-        },
-        set: function(name, value, options) {
-            options = (typeof(options) == 'object') ? options: {
-                minute: options
-            };
-            var arg_len = arguments.length;
-            var path = (arg_len > 3) ? arguments[3] : (options.path || '/');
-            var domain = (arg_len > 4) ? arguments[4] : (options.domain || (options.root ? this.getRootDomain() : ''));
-            var exptime = 0;
-            if (options.day) {
-                exptime = 1000 * 60 * 60 * 24 * options.day;
-            } else if (options.hour) {
-                exptime = 1000 * 60 * 60 * options.hour;
-            } else if (options.minute) {
-                exptime = 1000 * 60 * options.minute;
-            } else if (options.second) {
-                exptime = 1000 * options.second;
-            }
-            var exp = new Date(),
-                expires = '';
-            if (exptime > 0) {
-                exp.setTime(exp.getTime() + exptime);
-                expires = '; expires=' + exp.toGMTString();
-            }
-            domain = (domain) ? ('; domain=' + domain) : '';
-            document.cookie = name + '=' + escape(value || '') + '; path=' + path + domain + expires;
-        },
-        del: function(name, options) {
-            options = options || {};
-            var path = '; path=' + (options.path || '/');
-            var domain = (options.domain) ? ('; domain=' + options.domain) : '';
-            if (options.root) domain = '; domain=' + this.getRootDomain();
-            document.cookie = name + '=' + path + domain + '; expires=Thu,01-Jan-70 00:00:01 GMT';
-        }
-    });
-})(jQuery);
+//数组乱序
+if (!Array.prototype.shuffle) {
+    Array.prototype.shuffle = function() {
+        for(var j, x, i = this.length; i; j = parseInt(Math.random() * i), x = this[--i], this[i] = this[j], this[j] = x);
+        return this;
+    };
+}
+//另一种乱序
+var testArray=[1,2,3,4,5,6,7,8,9,10,22,33,55,77,88,99]; 
+testArray.sort(function(){return Math.random()>0.5?-1:1;}); 
+
+//搜索
+var testArray=['df', 'rtr', 'wy', 'dafd', 'dfs', 'wefa', 'tyr', 'rty', 'rty', 'ryt', 'afds', 'wer', 'te']; 
+testArray.sort(function(a,b){return a.indexOf('a')==-1?1:-1;});
+
+function lazyArrayDeal(arr,process){
+    var items=arr.cancat();
+    do{
+        var stime=+new Date();
+        var todo=items.shift();
+        process(todo);
+    }while((+new Date()-stime)>50&&items.length)
+    if(items.length){
+        setTimeout(arguments.callee,25);
+    }else{
+        console.log(todo);
+    }
+}
